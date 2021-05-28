deb4429627c91a0ca89d043a14112e6910defd54
Vishkash <vishkash.vishnu@gmail.com>
2021-05-28 14:34:53 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '74a706af-e4c3-4e13-aa4a-398598c3e88b';
+export const MODAL_CLOSE_SEC = 2.5;
